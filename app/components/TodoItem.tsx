e66228065db370e4ec16648a4b8268acd3f2a791
Artem Klibaner <32778606+z97@users.noreply.github.com>
2024-07-03 22:07:56 +0300

    Create TodoItem.tsx

---
+++ app/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface TodoItemProps {
+  todo: string;
+  onDelete: () => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete }) => (
+  <div>
+    <span>{todo}</span>
+    <button onClick={onDelete}>Delete</button>
+  </div>
+);
+
+export default TodoItem;
